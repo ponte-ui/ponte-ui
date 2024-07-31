a0ff231b0f1c4cc98091063ebec540689db54d4b
os-hun <shun12baseball@gmail.com>
2024-07-31 18:33:04 +0900

    fix: delete eslint-config packages.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,10 @@
-const { resolve } = require("node:path");
-
-const project = resolve(process.cwd(), "tsconfig.json");
-
-/*
- * This is a custom ESLint configuration for use a library
- * that utilizes React.
- *
- * This config extends the Vercel Engineering Style Guide.
- * For more information, see https://github.com/vercel/style-guide
- *
- */
-
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: [
     "@vercel/style-guide/eslint/browser",
     "@vercel/style-guide/eslint/typescript",
     "@vercel/style-guide/eslint/react",
   ].map(require.resolve),
-  parserOptions: {
-    project,
-  },
   plugins: ["only-warn"],
   globals: {
     JSX: true,
--- apps/docs/.eslintrc.cjs
+++ apps/docs/.eslintrc.cjs
@@ -1,4 +1,30 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["@repo/eslint-config/storybook.js"],
+  extends: [
+    "plugin:storybook/recommended",
+    "plugin:mdx/recommended",
+    ...[
+      "@vercel/style-guide/eslint/node",
+      "@vercel/style-guide/eslint/typescript",
+      "@vercel/style-guide/eslint/browser",
+      "@vercel/style-guide/eslint/react",
+    ].map(require.resolve),
+  ],
+  plugins: ["only-warn"],
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: ["node_modules/", "dist/"],
+  // add rules configurations here
+  rules: {
+    "import/no-default-export": "off",
+  },
 };
